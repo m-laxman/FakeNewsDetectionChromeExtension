1a0677ac7b5c4363f61b7138c61b1d8dab6486a7
shekharmnnit <shekhark221@gmail.com>
2024-03-24 11:08:28 -0400

    api added

--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     document.querySelector('.send-button').addEventListener('click', function() {
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            document.querySelector('.response').textContent = "cliicked";
+            document.querySelector('.response').textContent = "waiting for response...";
             document.querySelector('.response').style.color = "blue";
             var xhr = new XMLHttpRequest();
-            xhr.open("POST", "http://your-api-url.com", true);
+            xhr.open("POST", "http://localhost:8001/postjson", true);
             xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
             xhr.onreadystatechange = function() {
                 if (xhr.readyState == 4 && xhr.status == 200) {
